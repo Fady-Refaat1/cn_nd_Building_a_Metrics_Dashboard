9240be6a7d021d1410e62f4b0527ad5c7888aa8b
Fady Refaat <fadyrefaatzaky@gmail.com>
2022-08-25 16:19:17 +0200

    jeager

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,114 +1,120 @@
-import requests
-import logging
-import re
-import logging
-
-from os import getenv
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
+import time
+import random
+import pymongo
 from flask_pymongo import PyMongo
-from jaeger_client import Config
-from flask_opentracing import FlaskTracing
-
-from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
 from prometheus_flask_exporter import PrometheusMetrics
+from jaeger_client import Config
+import logging
+from os import getenv
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = "example-mongodb"
-app.config[
-    "MONGO_URI"
-] = "mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb"
+app.config['MONGO_DBNAME'] = 'example-mongodb'
+app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
 mongo = PyMongo(app)
-metrics = PrometheusMetrics(app)
+metrics = PrometheusMetrics(app, group_by='endpoint')
+
 # static information as metric
-metrics.info("app_info", "Application info", version="1.0.3")
+metrics.info('app_info', 'Application info', version='1.0.3')
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
-logging.getLogger("").handlers = []
-logging.basicConfig(format="%(message)s", level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+by_endpoint_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by request endpoint',
+    labels={'endpoint': lambda: request.endpoint}
+)
 
-JAEGER_HOST = getenv('JAEGER_HOST', 'localhost')
+JAEGER_AGENT_HOST = getenv('JAEGER_HOST', 'localhost')
+
+class InvalidHandle(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        error_message = dict(self.payload or ())
+        error_message['message'] = self.message
+        return error_message
+
+@app.route('/error')
+@by_endpoint_counter
+def oops():
+    return ':(', 500
 
 def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
     config = Config(
         config={
-            "sampler": {"type": "const", "param": 1},
-            "logging": True,
-            "local_agent": {'reporting_host': "simplest-query.observability.svc.cluster.local"},
-            "reporter_batch_size": 1,
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+            'local_agent': {'reporting_host': JAEGER_AGENT_HOST},
         },
         service_name=service,
-        validate=True,
-        metrics_factory=PrometheusMetricsFactory(service_name_label=service),
     )
 
     # this call also sets opentracing.tracer
     return config.initialize_tracer()
 
+tracer = init_tracer('backend')
 
-tracer = init_tracer("backend")
-flask_tracer = FlaskTracing(tracer, True, app)
+@app.errorhandler(InvalidHandle)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
-@app.route("/")
-def homepage():
-    return "Hello World"
+@app.route('/foo')
+@by_endpoint_counter
+def get_error():
+    raise InvalidHandle('error occur', status_code=410)
 
+@app.route('/')
+@by_endpoint_counter
+def homepage(): 
+    with tracer.start_span('hello-world'):
+        return "Hello World"
 
-@app.route("/api")
+@app.route('/api')
+@by_endpoint_counter
 def my_api():
-    answer = "something"
+    with tracer.start_span('api'):
+        answer = "something"
     return jsonify(repsonse=answer)
 
-
-@app.route("/star", methods=["POST"])
+# This will return 405 error
+@app.route('/star', methods=['POST'])
+@by_endpoint_counter
 def add_star():
-    star = mongo.db.stars
-    name = request.json["name"]
-    distance = request.json["distance"]
-    star_id = star.insert({"name": name, "distance": distance})
-    new_star = star.find_one({"_id": star_id})
-    output = {"name": new_star["name"], "distance": new_star["distance"]}
-    return jsonify({"result": output})
+  star = mongo.db.stars
+  name = request.json['name']
+  distance = request.json['distance']
+  star_id = star.insert({'name': name, 'distance': distance})
+  new_star = star.find_one({'_id': star_id })
+  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+  return jsonify({'result' : output})
 
-
-def test():
-    def remove_tags(text):
-        tag = re.compile(r"<[^>]+>")
-        return tag.sub("", text)
-
-    with tracer.start_span("get-python-jobs") as span:
-        res = requests.get("https://jobs.github.com/positions.json?description=python")
-        span.log_kv({"event": "get jobs count", "count": len(res.json())})
-        span.set_tag("jobs-count", len(res.json()))
-
-        jobs_info = []
-        for result in res.json():
-            jobs = {}
-            with tracer.start_span("request-site") as site_span:
-                logger.info(f"Getting website for {result['company']}")
-                try:
-                    jobs["description"] = remove_tags(result["description"])
-                    jobs["company"] = result["company"]
-                    jobs["company_url"] = result["company_url"]
-                    jobs["created_at"] = result["created_at"]
-                    jobs["how_to_apply"] = result["how_to_apply"]
-                    jobs["location"] = result["location"]
-                    jobs["title"] = result["title"]
-                    jobs["type"] = result["type"]
-                    jobs["url"] = result["url"]
-
-                    jobs_info.append(jobs)
-                    site_span.set_tag("http.status_code", res.status_code)
-                    site_span.set_tag("company-site", result["company"])
-                except Exception:
-                    logger.error(f"Unable to get site for {result['company']}")
-                    site_span.set_tag("http.status_code", res.status_code)
-                    site_span.set_tag("company-site", result["company"])
-
-    return jsonify(jobs_info)
+@app.route('/healthz')
+@by_endpoint_counter
+def healthcheck():
+    app.logger.info('Status request successfull')
+    return jsonify({"result": "OK - healthy"})
+    
 
 if __name__ == "__main__":
-    app.run()
-    test()
+    app.run(threaded=True)
